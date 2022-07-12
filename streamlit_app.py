5db6d73eafacd0da7c4a71155ef68083613dc36a
eddyAtUrbern <63434918+eddyAtUrbern@users.noreply.github.com>
2022-07-12 20:12:07 +1000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -473,7 +473,7 @@ if model == 'Marsh':
     df_sim = simulate_model(age = 0, weight = weight, height = 0, gender = 0, duration = duration, depth = depth, model = model)
     # df_sim = df_sim.drop(['Duration'], axis = 1)
     # result = df_sim['Volume'].iloc[-1]
-    # st.dataframe(df_sim)
+    st.dataframe(df_sim)
     st.download_button(
          label="Download data as CSV",
          data= df_sim.to_csv().encode('utf-8'),
